6ad4a908f98d15975abafe8efda832f7181c46db
MuchoPepito <declerck.jessy@gmail.com>
2017-12-02 15:11:22 +0100

    console in topmost mode

--- TreasureHuntHelper/Program.cs
+++ TreasureHuntHelper/Program.cs
@@ -11,15 +11,37 @@ using System.Threading;
 using TreasureHuntHelper.Web;
 using System.IO;
 using System.Reflection;
+using System.Runtime.InteropServices;
+using System.Diagnostics;
 
 namespace treasureHuntHelper
 {
     class Program
     {
+        [DllImport("user32.dll", SetLastError = true)]
+        [return: MarshalAs(UnmanagedType.Bool)]
+        private static extern bool SetWindowPos(
+       IntPtr hWnd,
+       IntPtr hWndInsertAfter,
+       int x,
+       int y,
+       int cx,
+       int cy,
+       int uFlags);
 
-        
+        private const int HWND_TOPMOST = -1;
+        private const int SWP_NOMOVE = 0x0002;
+        private const int SWP_NOSIZE = 0x0001;
+
         static void Main(string[] args)
         {
+            IntPtr hWnd = Process.GetCurrentProcess().MainWindowHandle;
+
+            SetWindowPos(hWnd,
+                new IntPtr(HWND_TOPMOST),
+                0, 0, 0, 0,
+                SWP_NOMOVE | SWP_NOSIZE);
+
             ProtocolTypeManager.Initialize();
             MessageReceiver.Initialize();
             string path = Path.Combine(Path.GetDirectoryName(Assembly.GetExecutingAssembly().Location), @"GamePath.txt");
